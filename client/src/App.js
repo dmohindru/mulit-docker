2060239082b8a769f69f1c28aefda375bfaea8a6
Dhruv Mohindru <dhruv.mohindru@parcelpoint.com.au>
2022-07-22 10:18:50 +1000

    Code refactor

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Fib Calculator
+            Fib Calculator version 3 dhruv
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
